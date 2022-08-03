89ec1e72019ba3f564d0b7c6ed366ed9bed54eb3
Brandon Saint-John <bsaintjo@ucsc.edu>
2022-08-02 20:02:43 -0700

    median

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,32 @@
-fn median(a: Vec<f32>) -> Option<f32> {
-    todo!();
+use std::cmp::Ordering;
+
+fn median(mut a: Vec<f32>) -> Option<f32> {
+    let mut contains_nan = false;
+    a.sort_by(|a, b| {
+        let cmp = a.partial_cmp(b);
+        if let Some(ord) = cmp {
+            ord
+        } else {
+            contains_nan = true;
+            Ordering::Equal
+        }
+    });
+
+    if contains_nan {
+        return None;
+    }
+
+    let len = a.len();
+    let idx = len / 2;
+
+    if a.is_empty() {
+        None
+    } else if len % 2 == 0 {
+        let avg = (a[idx] + a[idx - 1]) / 2.;
+        Some(avg)
+    } else {
+        Some(a[idx])
+    }
 }
 
 fn main() {
@@ -36,6 +63,14 @@ fn even_length() {
 fn unsorted_list() {
     let input = vec![1.0, 5.0, 2.0];
     let expected_output = Some(2.0);
+    let actual_output = median(input);
+    assert_eq!(actual_output, expected_output);
+}
+
+#[test]
+fn single_list() {
+    let input = vec![1.0];
+    let expected_output = Some(1.0);
     let actual_output = median(input);
     assert_eq!(actual_output, expected_output);
 }
