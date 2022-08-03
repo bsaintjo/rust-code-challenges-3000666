353c68b87e4b157b65afad1e346221d866f9ffee
Brandon Saint-John <bsaintjo@ucsc.edu>
2022-08-02 21:17:51 -0700

    unique

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,42 @@
-fn unique(a: Vec<i32>) -> Vec<i32> {
-    todo!();
+use std::collections::HashSet;
+use std::hash::Hash;
+
+fn unique(mut a: Vec<i32>) -> Vec<i32> {
+    a.sort();
+    a.dedup();
+    a
 }
 
 // advanced 1: use generic types
-// fn unique(a: Vec<T>) -> Vec<T> {
-//     todo!();
-// }
+fn unique_t<T: Ord>(mut a: Vec<T>) -> Vec<T> {
+    a.sort();
+    a.dedup();
+    a
+}
 
 // advanced 2: keep items in order
-// fn unique(a: Iterable<T>) -> Vec<T> {
-//     todo!();
-// }
+fn unique_retain<T: Eq + Hash + Clone>(a: impl Iterator<Item = T>) -> Vec<T> {
+    let mut b = Vec::new();
+    let mut seen = HashSet::new();
+    for x in a {
+        if seen.insert(x.clone()) {
+            b.push(x);
+        }
+    }
+    b
+}
 
 // advanced 3: use iterators
-// fn unique(a: Iterable<T>) -> Vec<T> {
-//     todo!();
-// }
+fn unique_iter<T: Eq + Hash + Clone>(a: impl Iterator<Item = T>) -> Vec<T> {
+    let mut seen = HashSet::new();
+    a.into_iter().filter(|x| seen.insert(x.clone())).collect()
+}
 
 fn main() {
     let input = vec![2, 1, 1];
     let answer = unique(input);
     println!("unique items -> {:?}", answer)
 }
-
 
 #[test]
 fn empty_list() {
@@ -48,7 +62,6 @@ fn unsorted_list() {
     assert_eq!(actual_output, expected_output);
 }
 
-
 #[test]
 fn unsorted_list_with_duplicates() {
     let input = vec![1, 5, 2, 2, 1];
@@ -60,7 +73,7 @@ fn unsorted_list_with_duplicates() {
 #[test]
 fn sorted_list_with_duplicates() {
     let mut input = vec![1, 5, 2, 2, 1];
-    input.sort_by(|x,y| x.partial_cmp(y).unwrap());
+    input.sort_by(|x, y| x.partial_cmp(y).unwrap());
     let expected_output = vec![1, 2, 5];
     let actual_output = unique(input);
     assert_eq!(actual_output, expected_output);
