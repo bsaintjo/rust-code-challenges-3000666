5067d52db3be4589aec684eff785e59e2e94c110
Brandon Saint-John <bsaintjo@ucsc.edu>
2022-08-10 00:03:31 -0700

    shortest_path

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::collections::{HashMap, HashSet};
+use std::{
+    collections::{HashMap, HashSet},
+    hash::Hash,
+};
 
 type Node = usize;
 type Cost = usize;
@@ -14,9 +17,7 @@ impl Graph {
         let mut nodes = HashSet::new();
 
         for &(source, destination, cost) in edge_list.iter() {
-            let destinations = adjacency_list
-                .entry(source)
-                .or_insert_with(|| Vec::new());
+            let destinations = adjacency_list.entry(source).or_insert_with(|| Vec::new());
 
             destinations.push((destination, cost));
 
@@ -31,17 +32,63 @@ impl Graph {
     }
 }
 
-
 fn shortest_path(g: &Graph, start: Node, goal: Node) -> Option<(Vec<Node>, Cost)> {
-    todo!()
+    let mut costs: HashMap<Node, Cost> = HashMap::new();
+    let mut parents: HashMap<Node, Node> = HashMap::new();
+    let mut processed: HashSet<Node> = HashSet::new();
+
+    processed.insert(start);
+
+    for &(n, c) in g.edges.get(&start).unwrap().iter() {
+        costs.insert(n, c);
+        parents.insert(n, start);
+    }
+    costs.insert(goal, usize::MAX);
+
+    while let Some(node) = find_lowest_cost_node(&costs, &processed) {
+        let cost = costs[&node];
+        for &(neighbor_node, neighbor_cost) in g.edges.get(&node).unwrap().iter() {
+            let new_cost = cost + neighbor_cost;
+            if !costs.contains_key(&neighbor_node) || costs[&neighbor_node] > new_cost {
+                costs.insert(neighbor_node, new_cost);
+                parents.insert(neighbor_node, node);
+            }
+        }
+        processed.insert(node);
+    }
+
+    let mut acc = vec![goal];
+    let total = costs[&goal];
+    parents.get(&goal).and_then(|p| {
+        let mut p = *p;
+        while let Some(&new_par) = parents.get(&p) {
+            acc.push(new_par);
+            p = new_par;
+            if p == start {
+                break;
+            }
+        }
+        if p == start {
+            Some((acc, total))
+        } else {
+            None
+        }
+    })
+}
+
+fn find_lowest_cost_node(costs: &HashMap<Node, Cost>, processed: &HashSet<Node>) -> Option<Node> {
+    costs
+        .iter()
+        .filter(|(node, _)| !processed.contains(node))
+        .min_by_key(|(_, &cost)| cost)
+        .map(|(&node, _)| node)
 }
 
 fn main() {
     let edge_list = include!("large_graph.in");
     let g = Graph::from_edge_list(&edge_list);
 
-    if let Some((path, cost)) = shortest_path(
-            &g, 1000, 9000) {
+    if let Some((path, cost)) = shortest_path(&g, 1000, 9000) {
         println!("1000->9000, {:?} {}", path, cost);
     };
 }
@@ -53,5 +100,5 @@ fn large_graph() {
 
     let path = shortest_path(&g, 1000, 9000);
     assert!(path.is_some());
-    assert_eq!(path.unwrap().1, 24); 
+    assert_eq!(path.unwrap().1, 24);
 }
